003fdb2eba82185ea52a1de2abb18e161a2e3f57
John Comeau <jc@unternet.net>
2026-03-22 19:39:28 -0700

    fixed date

--- location.sh
+++ location.sh
@@ -4,7 +4,7 @@
 # https://medium.com/@der.loste.kitkat/get-started-with-ish-b29add1f72a3
 while read line; do
  timestamp=$(date -u --iso-8601=ns)
- date=${timestamp##T*}
+ date=${timestamp%%T*}
  echo $date $timestamp $line
  sleep 15
 done < /dev/location
