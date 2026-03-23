35ce364ee89c1b3e34ded79568081107781aa38e
John Comeau <jc@unternet.net>
2026-03-23 02:50:58 +0000

    experimenting with it

--- location.sh
+++ location.sh
@@ -3,6 +3,8 @@
 # location being actively used.
 # https://medium.com/@der.loste.kitkat/get-started-with-ish-b29add1f72a3
 while read line; do
- echo $line
+ timestamp=$(date -u --iso-8601=ns)
+ date=${timestamp##T*}
+ echo $date $timestamp $line
  sleep 15
 done < /dev/location
