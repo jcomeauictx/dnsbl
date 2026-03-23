74fb3fb5b599bf87da077919a36e0b17ec7bd9d0
John Comeau <jc@unternet.net>
2026-03-22 19:47:17 -0700

    example output

--- location.sh
+++ location.sh
@@ -2,6 +2,8 @@
 # iOS keeps apps open in background only 30 seconds unless
 # location being actively used.
 # https://medium.com/@der.loste.kitkat/get-started-with-ish-b29add1f72a3
+# example output of /dev/location:
+# +24.163419,-110.311692
 while read line; do
  timestamp=$(date -u --iso-8601=ns)
  date=${timestamp%%T*}
