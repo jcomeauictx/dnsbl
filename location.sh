aee1a1b398a4fc9d97a890614ef9a5320adbf3cb
John Comeau <jc@unternet.net>
2026-03-24 16:56:36 +0000

    almost ready to log

--- location.sh
+++ location.sh
@@ -2,9 +2,16 @@
 # iOS keeps apps open in background only 30 seconds unless
 # location being actively used.
 # https://medium.com/@der.loste.kitkat/get-started-with-ish-b29add1f72a3
+DEBUGGING=$1
+LOGDIR=/var
+if [ ! -w $LOGDIR ]; then LOGDIR=$HOME/$LOGDIR; fi
+LOGDIR=$LOGDIR/location
+if [ "$DEBUGGING" ]; then echo LOGDIR=$LOGDIR >&2; fi
+mkdir -p $LOGDIR
 while read line; do
  timestamp=$(date -u --iso-8601=ns)
  date=${timestamp%%T*}
- echo $date $timestamp $line
+ if [ "$DEBUGGING" ]; then echo $date $timestamp $line >&2; fi
+ echo $timestamp ${line%%,*} ${line##*,}
  sleep 15
 done < /dev/location
