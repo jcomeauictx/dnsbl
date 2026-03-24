d584ee5e5dae40ca052e594f606db7d7d81a24b7
John Comeau <jc@unternet.net>
2026-03-24 19:56:14 +0000

    maybe works

--- location.mk
+++ location.mk
@@ -1,6 +1,9 @@
+SHELL := /bin/bash
 install: /etc/init.d/location
-/etc/init.d/%: %
-	cp -i $< $@
-	chmod 0744 $@
-	rc-update add $* default
-	rc-service $* start
+/etc/init.d/%: % /root/.local/bin/location.sh
+	sudo cp -i $< $@
+	sudo chmod 0744 $@
+	sudo rc-update add $* default
+	sudo rc-service $* start
+/root/.local/bin/%: %
+	sudo ln -sf $(PWD)/$* $@
